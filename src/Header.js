467ab60b5c35649408372354f4b97c5c899ebab5
Asutosh kumar <ashuu.barnwal@gmail.com>
2021-12-21 17:45:13 +0530

    ref to each button

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,45 @@
+import { useState } from "react";
+
 const Header = ({setCat, inputRef}) => {
+    const [input1, setInput1] = useState(null);
+    const [input2, setInput2] = useState(null);
+    const [input3, setInput3] = useState(null);
+
     return (
         <header>
             <button 
                 autoFocus
-                ref={inputRef}
+                ref={input1}
                 className="btn"
-                onClick={()=>setCat('users')}
+                onClick={ () => {
+                    setCat('users');
+                    console.log('clicked users');
+                    setInput1(inputRef);
+                    setInput2(null);
+                    setInput3(null);
+                }}
             >users</button>
             <button 
-                ref={inputRef}
                 className="btn"
-                onClick={()=>setCat('posts')}
+                ref={input2}
+                onClick={ () => {
+                    setCat('posts');
+                    setInput1(null);
+                    setInput2(inputRef);
+                    setInput3(null);
+                    console.log('clicked posts');
+                }}
             >posts</button>
             <button
-                ref={inputRef}
                 className="btn"
-                onClick={()=>setCat('comments')}
+                ref={input3}
+                onClick={ () => {
+                    setCat('comments');
+                    setInput1(null);
+                    setInput2(null);
+                    setInput3(inputRef);
+                    console.log('clicked comments');
+                }}
             >comments</button> 
         </header>
     )
